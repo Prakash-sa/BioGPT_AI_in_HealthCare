133fc970dab244cae2b5063358bde81e88460621
Prakash-sa <sainiprakash525@gmail.com>
2023-02-20 15:25:39 -0600

    completed the chat interface

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,9 @@
-import React,{useCallback} from 'react'
+import React,{useCallback, useRef} from 'react'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+
+import human from "../images/human.png"
+import robot from '../images/robot.png'
 
 import {
   MDBContainer,
@@ -22,8 +25,35 @@ const backendLink="http://0c00-34-74-16-171.ngrok.io"
 
 const Home = () => {
 
+  const dummy = useRef(null);
+  
+
     const [question, setQuestion] = useState('');
-    const [data,setData]=useState({});
+    const [data,setData]=useState(
+      [
+        {
+          "id":"ai",
+          "text":"Please enter the context.."
+        },
+        {
+          "id":"client",
+          "text":"this is the context"
+        },
+        {
+          "id":"ai",
+          "text":"Hi there how are you doing?"
+        },
+        {
+          "id":"client",
+          "text":"Fuck off AI."
+        }
+      ]
+    );
+    const [inputText,setInputText]=useState('');
+
+    useEffect(() => {
+      dummy.current.scrollIntoView({ behavior: "smooth" });
+    }, [data]);
 
 
     function isEmptyObject(obj){
@@ -31,12 +61,16 @@ const Home = () => {
     }
 
     const onSendClick=()=>{
-      onFormSubmit();
+      console.log(inputText);
+      const tmp={"id":"client","text":inputText};
+      setData([...data,tmp]);
+      setInputText("");
+      // onFormSubmit();
     }
 
   
     const onFormSubmit = (e) => {
-      e.preventDefault();
+      // e.preventDefault();
       const config={
         headers:{
           'content-type':'application/json',
@@ -55,99 +89,84 @@ const Home = () => {
         console.log('err',err);
       });
     };
+
+
+    const listItems = data.map(
+      (element) => {
+          return (
+            element.id=='ai'?
+              <li className="d-flex justify-start mb-4">
+                <img
+                  src={robot}
+                  alt="avatar"
+                  className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
+                  width="60"
+                />
+                <MDBCard class="mask-custom">
+                  <MDBCardHeader
+                    className="d-flex justify-content-between p-3"
+                    style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+                  >
+                    <p className="fw-bold mb-0">BioGPT</p>
+                    <p className="text-light small mb-0">
+                      <MDBIcon far icon="clock" /> 12 mins ago
+                    </p>
+                  </MDBCardHeader>
+                  <MDBCardBody>
+                    <p className="mb-0">
+                      {element.text}
+                    </p>
+                  </MDBCardBody>
+                </MDBCard>
+              </li>
+
+              :
+              <li className="d-flex justify-end mb-4">
+                <MDBCard className="w-100 " class="mask-custom">
+                  <MDBCardHeader
+                    className="d-flex justify-content-between p-3"
+                    style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+                  >
+                    <p class="fw-bold mb-0">You</p>
+                    <p class="text-light small mb-0">
+                      <MDBIcon far icon="clock" /> 13 mins ago
+                    </p>
+                  </MDBCardHeader>
+                  <MDBCardBody>
+                    <p className="mb-0">
+                      {element.text}
+                    </p>
+                  </MDBCardBody>
+                </MDBCard>
+                <img
+                  src={human}
+                  alt="avatar"
+                  className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong"
+                  width="60"
+                />
+              </li>
+          )
+      }
+  )
   
   return (
     
-    <MDBContainer fluid class="py-5 h-full w-full flex justify-center place-items-center align-middle items-center gradient-custom">
-      <MDBRow>
+    <MDBContainer fluid class="py-5 h-full w-full flex justify-center place-items-center align-middle gradient-custom">
+      <MDBRow class="py-5 h-full w-full flex justify-center place-items-center align-middle">
         <MDBCol md="6" lg="7" xl="8">
           <MDBTypography listUnStyled className="text-white">
-            <li className="d-flex justify-content-between mb-4">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                alt="avatar"
-                className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-                width="60"
-              />
-              <MDBCard class="mask-custom">
-                <MDBCardHeader
-                  className="d-flex justify-content-between p-3"
-                  style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                >
-                  <p className="fw-bold mb-0">Brad Pitt</p>
-                  <p className="text-light small mb-0">
-                    <MDBIcon far icon="clock" /> 12 mins ago
-                  </p>
-                </MDBCardHeader>
-                <MDBCardBody>
-                  <p className="mb-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </MDBCardBody>
-              </MDBCard>
-            </li>
-            <li class="d-flex justify-content-between mb-4">
-              <MDBCard className="w-100 " class="mask-custom">
-                <MDBCardHeader
-                  className="d-flex justify-content-between p-3"
-                  style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                >
-                  <p class="fw-bold mb-0">Lara Croft</p>
-                  <p class="text-light small mb-0">
-                    <MDBIcon far icon="clock" /> 13 mins ago
-                  </p>
-                </MDBCardHeader>
-                <MDBCardBody>
-                  <p className="mb-0">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium.
-                  </p>
-                </MDBCardBody>
-              </MDBCard>
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
-                alt="avatar"
-                className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong"
-                width="60"
-              />
-            </li>
-            <li className="d-flex justify-content-between mb-4">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                alt="avatar"
-                className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-                width="60"
-              />
-              <MDBCard class="mask-custom">
-                <MDBCardHeader
-                  className="d-flex justify-content-between p-3"
-                  style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                >
-                  <p className="fw-bold mb-0">Brad Pitt</p>
-                  <p className="text-light small mb-0">
-                    <MDBIcon far icon="clock" /> 10 mins ago
-                  </p>
-                </MDBCardHeader>
-                <MDBCardBody>
-                  <p className="mb-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                </MDBCardBody>
-              </MDBCard>
-            </li>
+            {listItems}
             <li className="mb-3">
-              <MDBTextArea label="Message" id="textAreaExample" rows={4} />
+              <MDBTextArea label="Message" id="textAreaExample" rows={4} value={inputText} onChange={(e)=>setInputText(e.target.value)} />
             </li>
-            <MDBBtn color="light" size="lg" rounded className="float-end">
+            <MDBBtn color="light" size="lg" rounded className="float-end" onClick={onSendClick}>
               Send
             </MDBBtn>
+            <div ref={dummy}></div>
           </MDBTypography>
         </MDBCol>
       </MDBRow>
+      
     </MDBContainer>
   )
 }
